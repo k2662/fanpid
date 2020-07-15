261a6b1c87856a85a4701ec65de7fb5f6cad8b21
Christopher Boyle <chrismjboyle@gmail.com>
2020-07-15 15:53:24 +0100

    Add fan manager object

---
+++ src/fan.cpp
@@ -0,0 +1,58 @@
+#include "fan.hpp"
+
+Fan::Fan(int index)
+  : selector(char(index+48)), control_mode(false)
+{
+
+  // GEt this fan's minimum RPM
+  char key[5] = {'F', 'M', selector, 'n'};
+  this->min = this->smc.read(key);
+
+  // Get this fan's maximum RPM
+  key[3] = 'x';
+  this->max = this->smc.read(key);
+}
+
+Fan::~Fan()
+{
+  // Set F0Md -> 0
+  // Set fan control mode back to auto mode
+  this->set_control_mode(false);
+}
+
+void Fan::init_control()
+{
+  this->set_control_mode(true);
+}
+
+void Fan::set_control_mode(bool value)
+{
+  if (value == this->control_mode)
+    return;
+
+  char key[5] = {'F', 'M', this->selector, 'd'};
+
+  try {
+    // Set control mode of the fan: 0 is auto, 1 is manual.
+    this->smc.write(key, int(value));
+  }
+  catch (const SMCError &e) {
+    throw SMCError("Could not set control mode: are you running as root?");
+  }
+
+  this->control_mode = value;
+}
+
+void Fan::set_target(double target)
+{
+  if (target < this->min)
+    target = this->min;
+
+  if (target > this->max)
+    target = this->max;
+
+  this->target = target;
+
+  char key[5] = {'F', 'M', selector, 'd'};
+  this->smc.write(key, target);
+}
---
+++ src/fan.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <smcpp/smc.hpp>
+
+class Fan {
+  private:
+    SMC smc;
+    char selector;
+    double min, max, target;
+    bool control_mode;
+
+
+  public:
+    Fan(int index);
+    ~Fan();
+
+    void init_control();
+    void set_control_mode(bool value=true);
+    void set_target(double value);
+};
